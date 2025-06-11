a7c1b87983fe87621213b453d2671d79d16b8a48
Braham <152489943+BrahamjeetRandhawa@users.noreply.github.com>
2025-06-11 23:08:05 +0200

    Update

--- terabyte/src/App.js
+++ terabyte/src/App.js
@@ -1,12 +1,18 @@
 
 import Top from './Top';
+import Mainpage from './Mainpage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
-      <Top />
+      <header className="App-header">
+        <Top className= "Topnav" />
+      </header>
+      <main className="App-main">
+      
+      <Mainpage className= "Mainpageterabyte" />
+      </main>
       
     </div>
   );
---
+++ terabyte/src/Mainpage.js
@@ -0,0 +1,16 @@
+import './Mainpagestyle.css';
+
+
+function Mainpage() {
+    return (
+        <div className="Mainpage">
+            <h2>Next time in Terabyte</h2>
+            <p>will the team hold up for this assignment?</p>
+            <p>Will the team be able to deliver?</p>
+            <p>Will the team be able to meet the deadline?</p>
+            <p>All of this in a the new episode of Terabyte delivers</p>
+        </div>
+    );
+}
+
+export default Mainpage;
